f8e207dba713b934cf5cbc8b271f8be016ac76bb
mayrazan <mayramanchein@yahoo.com.br>
2020-10-30 13:31:01 -0300

    Adding levels for the game

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const buttonInitGame = document.querySelector(".btn-init-game");
+const divButtons = document.querySelector(".container-buttons");
+const buttonEasy = document.querySelector(".btn-easy-level");
+const buttonMedium = document.querySelector(".btn-medium-level");
+const buttonHard = document.querySelector(".btn-hard-level");
 
 buttonInitGame.addEventListener("click", function () {
   buttonInitGame.remove();
@@ -6,7 +10,40 @@ buttonInitGame.addEventListener("click", function () {
 });
 
 function initGame() {
+  divButtons.style.display = "flex";
+
+  buttonEasy.addEventListener("click", function () {
+    removeButtons();
+    easyLevel();
+  });
+
+  buttonMedium.addEventListener("click", function () {
+    removeButtons();
+    mediumLevel();
+  });
+
+  buttonHard.addEventListener("click", function () {
+    removeButtons();
+    hardLevel();
+  });
+}
+
+function easyLevel() {
+  setInterval(createBalloon, 1500);
+}
+
+function mediumLevel() {
+  setInterval(createBalloon, 1000);
+}
+
+function hardLevel() {
   setInterval(createBalloon, 500);
+}
+
+function removeButtons() {
+  buttonEasy.remove();
+  buttonMedium.remove();
+  buttonHard.remove();
 }
 
 const balloonsContainer = document.querySelector(".container-balloons");
